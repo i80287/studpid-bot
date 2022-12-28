e565c571793ff34e063c482aedbbc850042944cd
i80287 <92759021+i80287@users.noreply.github.com>
2022-12-28 23:39:24 +0300

    replace parse_tools

--- storebot/storebot.py
+++ storebot/storebot.py
@@ -18,7 +18,7 @@ class StoreBot(Bot):
 
 if __name__ == "__main__":
     bot = StoreBot()
-    bot.load_extensions_from_module("Commands", ignore=["Commands.parse_tools"])
+    bot.load_extensions_from_module("Commands")
 
     if DEBUG:
         from colorama import Fore
