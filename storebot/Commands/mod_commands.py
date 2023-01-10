20878554824aaf65ecd6ab4489cb6ab58f236259
i80287 <92759021+i80287@users.noreply.github.com>
2023-01-10 09:45:46 +0300

    update mod commands cog

--- storebot/Commands/mod_commands.py
+++ storebot/Commands/mod_commands.py
@@ -2,16 +2,18 @@ from typing import Literal
 from sqlite3 import connect
 from contextlib import closing
 
-from nextcord import Embed, Locale, Interaction, slash_command
+from nextcord import Embed, Locale, Interaction, User, Member, slash_command
+from nextcord.ui import Button, StringSelect
 from nextcord.ext import application_checks
-from nextcord.ext.commands import Cog, Bot
+from nextcord.ext.commands import Cog
 
+from storebot import StoreBot
 from Variables.vars import CWD_PATH
 from CustomComponents.custom_views import SettingsView
 
 
 class ModCommandsCog(Cog):
-    settings_text = {
+    settings_text: dict[int, dict[int, str | list[str]]] = {
         0 : {
             0 : "Choose section",
             1: [
@@ -20,7 +22,7 @@ class ModCommandsCog(Cog):
                 "<:user:1002245779089535006> manage members",
                 "💰 economy",
                 "📈 ranking",
-                "📊 polls"
+                "📊 polls",
             ],
         },
         1 : {
@@ -31,18 +33,20 @@ class ModCommandsCog(Cog):
                 "<:user:1002245779089535006> управление пользователями",
                 "💰 экономика",
                 "📈 ранговая система",
-                "📊 поллы"
+                "📊 поллы",
             ],
         }
     }
 
-    def __init__(self, bot: Bot) -> None:
-        self.bot: Bot = bot
+    def __init__(self, bot: StoreBot) -> None:
+        self.bot: StoreBot = bot
 
     @staticmethod
     def mod_check(interaction: Interaction) -> bool:
-        u = interaction.user
-        if u.guild_permissions.administrator or u.guild_permissions.manage_guild:
+        member: User | Member | None = interaction.user
+        if not member or not isinstance(member, Member):
+            return False
+        if member.guild_permissions.administrator or member.guild_permissions.manage_guild:
             return True
 
         with closing(connect(f'{CWD_PATH}/bases/bases_{interaction.guild_id}/{interaction.guild_id}.db')) as base:
@@ -51,20 +55,8 @@ class ModCommandsCog(Cog):
                 if mod_roles_ids_list:
                     mod_roles_ids_set: set[int] = {x[0] for x in mod_roles_ids_list}
                     del mod_roles_ids_list
-                    return any(role.id in mod_roles_ids_set for role in u.roles)
+                    return any(role.id in mod_roles_ids_set for role in member.roles)
                 return False
-
-    async def settings(self, interaction: Interaction) -> None:
-        lng: Literal[1, 0] = 1 if "ru" in interaction.locale else 0
-        st_view: SettingsView = SettingsView(t_out=120, auth_id=interaction.user.id, bot=self.bot)
-        emb: Embed = Embed(title=self.settings_text[lng][0], description="\n".join(self.settings_text[lng][1]))
-        await interaction.response.send_message(embed=emb, view=st_view)
-
-        await st_view.wait()
-        for c in st_view.children:
-            c.disabled = True
-        await interaction.edit_original_message(view=st_view)
-
 
     @slash_command(
         name="settings",
@@ -74,9 +66,18 @@ class ModCommandsCog(Cog):
         }
     )
     @application_checks.check(mod_check)
-    async def settings_e(self, interaction: Interaction) -> None:
-        await self.settings(interaction=interaction)
+    async def settings(self, interaction: Interaction) -> None:
+        lng: Literal[1, 0] = 1 if "ru" in str(interaction.locale) else 0
+        st_view: SettingsView = SettingsView(t_out=120, auth_id=interaction.user.id, bot=self.bot)
+        emb: Embed = Embed(title=self.settings_text[lng][0], description="\n".join(self.settings_text[lng][1]))
+        await interaction.response.send_message(embed=emb, view=st_view)
+
+        await st_view.wait()
+        for c in st_view.children:
+            if isinstance(c, Button) or isinstance(c, StringSelect):
+                c.disabled = True
+        await interaction.edit_original_message(view=st_view)
     
 
-def setup(bot: Bot) -> None:
+def setup(bot: StoreBot) -> None:
     bot.add_cog(ModCommandsCog(bot))
