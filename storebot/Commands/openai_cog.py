eb6b6f77a0aa704211e9ddc292f20a37e3d9f884
i80287 <92759021+i80287@users.noreply.github.com>
2023-02-19 13:52:14 +0300

    update openai cog

--- storebot/Commands/openai_cog.py
+++ storebot/Commands/openai_cog.py
@@ -1,10 +1,8 @@
 import re
 import openai
 from asyncio import Lock
-from typing import (
-    Literal,
-    Dict
-)
+if __debug__:
+    from typing import Literal
 
 from nextcord import (
     slash_command,
@@ -14,35 +12,41 @@ from nextcord import (
     Embed
 )
 from nextcord.ext.commands import Cog
+from nextcord.channel import TextChannel
+from nextcord.threads import Thread
 
 from storebot import StoreBot
 from Tools.logger import Logger
-from Variables.vars import BANNED_WORDS
-from config import OPENAI_API_KEY
 
+import config
+from Variables import vars
+openai.api_key = config.OPENAI_API_KEY
+words_filter: re.Pattern[str] = re.compile(
+    pattern=vars.BANNED_WORDS,
+    flags=re.RegexFlag.IGNORECASE | re.RegexFlag.MULTILINE
+)
+del config
+del vars
 
 class OpenAICog(Cog):
     openai_lock: Lock = Lock()
-    openai_cog_text: Dict[int, Dict[int, str]] = {
+    openai_cog_text: dict[int, dict[int, str]] = {
         0: {
             0: "**`Your query contains strange words...`**",
             1: "**`Question:`**\n{0}\n**`OpenAI thinks what to respond...`**",
             2: "**`Answer:`**\n{0}",
+            3: "**`Sorry, but this command can't be used in this channel. Please, try use it in common text channel or thread`**",
         },
         1: {
             0: "**`В Вашем запросе есть странные слова...`**",
             1: "**`Вопрос:`**\n{0}\n**`OpenAI думает, что ответить...`**",
             2: "**`Ответ:`**\n{0}",
+            3: "**`Извините, но эта команда не может быть использована в этом канале. Пожалуйста, попробуйте использовать её в обычном текстовом канале или ветке`**",
         }
     }
-    words_filter: re.Pattern[str] = re.compile(
-        pattern=BANNED_WORDS,
-        flags=re.RegexFlag.IGNORECASE | re.RegexFlag.MULTILINE
-    )
 
     def __init__(self, bot: StoreBot) -> None:
         self.bot: StoreBot = bot
-        openai.api_key = OPENAI_API_KEY
     
     @slash_command(
         name="ask",
@@ -72,23 +76,35 @@ class OpenAICog(Cog):
         assert interaction.user is not None
         assert interaction.guild is not None
         lng: Literal[1, 0] = 1 if "ru" in str(interaction.locale) else 0
-        if self.words_filter.findall(question):
+        if not isinstance(channel := interaction.channel, (TextChannel, Thread)):
+            await interaction.response.send_message(embed=Embed(description=self.openai_cog_text[lng][3]))
+            return       
+        
+        if words_filter.findall(question):
             await interaction.response.send_message(embed=Embed(description=self.openai_cog_text[lng][0]))
             return
         
         await interaction.response.send_message(embed=Embed(description=self.openai_cog_text[lng][1].format(question)))
-        async with self.openai_lock:
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt=question,        
-                temperature=0.6,
-                max_tokens=1984,
-                top_p=1,
-                frequency_penalty=0.5,
-                presence_penalty=0.0
-            )
+        async with channel.typing():
+            async with self.openai_lock:
+                response = openai.Completion.create(
+                    model="text-davinci-003",
+                    prompt=question,        
+                    temperature=0.6,
+                    max_tokens=1984,
+                    top_p=1,
+                    frequency_penalty=0.5,
+                    presence_penalty=0.0
+                )
+
         answer: str = response.choices[0].text.strip('\n') # type: ignore
-        await interaction.followup.send(embed=Embed(description=self.openai_cog_text[lng][2].format(answer)))
+        length: int = len(answer)
+        if length < 3900:
+            await interaction.followup.send(embed=Embed(description=self.openai_cog_text[lng][2].format(answer)))
+        else:
+            await interaction.followup.send(embed=Embed(description=self.openai_cog_text[lng][2].format(answer[:3900])))
+            await interaction.followup.send(embed=Embed(description=answer[3900:]))
+
         await Logger.write_log_async(
             "openai_logs.log",
             f"member {interaction.user.id}:{interaction.user.name}",
--- storebot/Variables/vars.py
+++ storebot/Variables/vars.py
@@ -1,3 +1,3 @@
 import os
 CWD_PATH: str = os.getcwd()
-BANNED_WORDS: str = r"(ass)|(stupid)|(fuck)|(dick)|(kill)|(dynamite)|(bomb)|(death)|(dead)|(terror)|(war)|(pron)|(porn)|(sex)|(убит)|(динамит)|(бомб)|(смерт)|(терро)|(войн)|(пизд)|(ебл)|(лох)|(жоп)|(даун)|(идиот)|(дур[ао])|(прон)|(порн)|(секс)|(пенис)|(член)"
+BANNED_WORDS: str = r"(ass)|(stupid)|(fuck)|motherf[a-zA-Z][ck]|(dick)|(kill)|(dynamite)|(bomb)|(death)|(dead)|(terror)|(war)|(pron)|(porn)|(sex)|(убит)|(динамит)|(бомб)|(смерт)|(терро)|(войн)|(пизд)|(ебл)|(лох)|(жоп)|(даун)|(идиот)|(дур[ао])|(прон)|(порн)|(секс)|(пенис)|(член)"
