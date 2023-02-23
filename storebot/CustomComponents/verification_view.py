13b01e73612d7007f080c06c26a6af18881c5de5
i80287 <92759021+i80287@users.noreply.github.com>
2023-02-23 19:32:58 +0300

    add verification view

---
+++ storebot/CustomComponents/verification_view.py
@@ -0,0 +1,32 @@
+from os import urandom
+
+from nextcord import (
+    ButtonStyle,
+    Interaction
+)
+
+from CustomComponents.view_base import ViewBase
+from CustomComponents.custom_button import CustomButton
+
+class VerificationView(ViewBase):
+    def __init__(self, author_id: int, timeout: float | None = 30.0) -> None:
+        super().__init__(0, author_id, timeout)
+        self.add_item(CustomButton(
+            style=ButtonStyle.green,
+            custom_id=f"0_{author_id}_" + urandom(4).hex(),
+            emoji="✅"
+        ))
+        self.add_item(CustomButton(
+            style=ButtonStyle.red,
+            custom_id=f"1_{author_id}_" + urandom(4).hex(),
+            emoji="❌"
+        ))
+        self.approved: bool | None = None
+    
+    async def click_button(self, interaction: Interaction, custom_id: str) -> None:
+        await interaction.response.defer()
+        self.approved = True if custom_id[0] == "0" else False
+        self.stop()
+    
+    async def click_select_menu(self, interaction: Interaction, custom_id: str, values: list[str]) -> None:
+        return
