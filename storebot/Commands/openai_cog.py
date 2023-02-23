3923c39e053bb60552d00b6b41653d92c5e9b7d7
i80287 <92759021+i80287@users.noreply.github.com>
2023-02-23 15:17:29 +0300

    update openai cog

--- storebot/Commands/openai_cog.py
+++ storebot/Commands/openai_cog.py
@@ -1,6 +1,6 @@
 import re
 import openai
-from asyncio import Lock
+from asyncio import Lock, sleep
 if __debug__:
     from typing import Literal
 
@@ -36,12 +36,16 @@ class OpenAICog(Cog):
             1: "**`Question:`**\n{0}\n**`OpenAI thinks what to respond...`**",
             2: "**`Answer:`**\n{0}",
             3: "**`Sorry, but this command can't be used in this channel. Please, try use it in common text channel or thread`**",
+            4: "**`System was not able to answer your question`**",
+            5: "**`An error occurred during sending response`**"
         },
         1: {
             0: "**`В Вашем запросе есть странные слова...`**",
             1: "**`Вопрос:`**\n{0}\n**`OpenAI думает, что ответить...`**",
             2: "**`Ответ:`**\n{0}",
             3: "**`Извините, но эта команда не может быть использована в этом канале. Пожалуйста, попробуйте использовать её в обычном текстовом канале или ветке`**",
+            4: "**`Системе не удалось ответить на Ваш вопрос`**",
+            5: "**`При отправке ответа произошла ошибка`**"
         }
     }
 
@@ -87,31 +91,49 @@ class OpenAICog(Cog):
         await interaction.response.send_message(embed=Embed(description=self.openai_cog_text[lng][1].format(question)))
         async with channel.typing():
             async with self.openai_lock:
-                response = openai.Completion.create(
-                    model="text-davinci-003",
-                    prompt=question,        
-                    temperature=0.6,
-                    max_tokens=1984,
-                    top_p=1,
-                    frequency_penalty=0.5,
-                    presence_penalty=0.0
-                )
+                try:
+                    response = openai.Completion.create(
+                        model="text-davinci-003",
+                        prompt=question,        
+                        temperature=0.6,
+                        max_tokens=1984,
+                        top_p=1,
+                        frequency_penalty=0.5,
+                        presence_penalty=0.0
+                    )
+                except Exception as ex:
+                    await Logger.write_one_log_async(
+                        filename="error.log",
+                        report=f"[FATAL] [ERROR] [ask] [question: {question}] [guild: {interaction.guild_id}:{interaction.guild.name}] [{str(ex)}]\n"
+                    )
+                    await interaction.followup.send(embed=Embed(description=self.openai_cog_text[lng][4]), ephemeral=True)
+                    return
 
         answer: str = response.choices[0].text.strip('\n') # type: ignore
         length: int = len(answer)
-        if length < 3900:
-            await channel.send(embed=Embed(description=self.openai_cog_text[lng][2].format(answer)))
+        try:
+            if length < 3900:
+                await channel.send(embed=Embed(description=self.openai_cog_text[lng][2].format(answer)))
+            else:
+                await channel.send(embed=Embed(description=self.openai_cog_text[lng][2].format(answer[:3900])))
+                for i in range(1, (length + 3899) // 3900 + 1):
+                    if (ans_part := answer[(3900 * i):min(3900 * (i + 1), length)]):
+                        await sleep(1.0)
+                        await channel.send(embed=Embed(description=ans_part))
+        except Exception as ex:
+            await Logger.write_one_log_async(
+                filename="error.log",
+                report=f"[FATAL] [ERROR] [ask] [question: {question}] [answer: {answer}] [guild: {interaction.guild_id}:{interaction.guild.name}] [{str(ex)}]\n"
+            )
+            await interaction.followup.send(embed=Embed(description=self.openai_cog_text[lng][5]), ephemeral=True)
         else:
-            await channel.send(embed=Embed(description=self.openai_cog_text[lng][2].format(answer[:3900])))
-            await channel.send(embed=Embed(description=answer[3900:]))
-
-        await Logger.write_log_async(
-            "openai_logs.log",
-            f"member {interaction.user.id}:{interaction.user.name}",
-            f"guild {interaction.guild_id}:{interaction.guild.name}",
-            question,
-            answer
-        )
+            await Logger.write_log_async(
+                "openai_logs.log",
+                f"member {interaction.user.id}:{interaction.user.name}",
+                f"guild {interaction.guild_id}:{interaction.guild.name}",
+                question,
+                answer
+            )
 
 
 def setup(bot: StoreBot) -> None:
--- storebot/Variables/vars.py
+++ storebot/Variables/vars.py
@@ -1,3 +1,55 @@
 import os
 CWD_PATH: str = os.getcwd()
-BANNED_WORDS: str = r"(ass)|(stupid)|(fuck)|motherf[a-zA-Z][ck]|(dick)|(kill)|(dynamite)|(bomb)|(death)|(dead)|(terror)|(war)|(pron)|(porn)|(sex)|(убит)|(динамит)|(бомб)|(смерт)|(терро)|(войн)|(пизд)|(ебл)|(лох)|(жоп)|(даун)|(идиот)|(дур[ао])|(прон)|(порн)|(секс)|(пенис)|(член)"
+BANNED_WORDS: str = r"""
+(relig)|
+(slave)|
+(war)|
+(blood)|
+(traum)|
+(violence)
+(weapon)|
+(kill)|
+(dynamite)|
+(bomb)|
+(death)|
+(dead)|
+(terror)|
+(destoy)|
+(drug)|
+(dirty old man)|
+(down'?s? syndrome)|
+(dwarf)|
+(duffer)|
+(hussy)|
+(ass)|
+(stupid)|
+(fuck)|
+(motherf[0-9a-z]*)|
+(dick)|
+(pron)|
+(porn)|
+(sex)|
+(раб)|
+(религ)|
+(оруж)|
+(кров[0-9a-z]+)|
+(убит)|
+(динамит)|
+(бомб)|
+(смерт)|
+(терро)|
+(войн)|
+(разруш)|
+(нарко)|
+(пизд)|
+([её]бл)|
+(лох)|
+(жоп)|
+(даун)|
+(идиот)|
+(дур[ао])|
+(прон)|
+(порн)|
+(секс)|
+(пенис)|
+(член)""".replace("\r\n", "").replace('\n', "")
