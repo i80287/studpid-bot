88f3963fa73bde6ffeb0cc8d2144c2de8c3a21e4
i80287 <92759021+i80287@users.noreply.github.com>
2023-03-28 16:48:11 +0300

    update emoji parse function

--- storebot/Tools/parse_tools.pyx
+++ storebot/Tools/parse_tools.pyx
@@ -1,31 +1,36 @@
 import re
+
 from emoji import demojize
 from nextcord import Emoji
 from nextcord.ext.commands import Bot
 
+
 cdef:
-    _CUSTOM_EMOJI_ID_PATTERN = re.compile(r"\d+", flags=re.RegexFlag.MULTILINE | re.RegexFlag.IGNORECASE)
-    _DEFAULT_EMOJI_PATTERN = re.compile(r":[A-Za-z\d_]+:", flags=re.RegexFlag.MULTILINE | re.RegexFlag.IGNORECASE)
+    findall_id = re.compile(r"\d+", flags=re.RegexFlag.MULTILINE | re.RegexFlag.IGNORECASE).findall
+    findall_emoji = re.compile(r":[A-Za-z\d_]+:", flags=re.RegexFlag.MULTILINE | re.RegexFlag.IGNORECASE).findall
 
 def parse_emoji(bot: Bot, str string) -> Emoji | str | None:
-    if string.isdigit():
+    if string.isdecimal() is True:
         emoji = bot.get_emoji(int(string))
-        if emoji:
+        if emoji is not None:
             return emoji
 
     cdef:
-        list finds = _CUSTOM_EMOJI_ID_PATTERN.findall(string)
+        list finds = findall_id(string)
         size_t length = len(finds)
         size_t i = 0
-    
-    if length:
-        while (i < length):
-            emoji = bot.get_emoji(int(finds[i]))
-            if emoji:
-                return emoji
-            i += 1
 
-    finds = _DEFAULT_EMOJI_PATTERN.findall(demojize(string))
-    if finds:
-        return finds[0]
+    # More predicted branch, but we dont want
+    # to call slow <demojize> function first
+    if length == 0:
+        finds = findall_emoji(demojize(string))
+        return finds[0] if finds else None
+
+    get_emoji = bot.get_emoji
+    while (i < length):
+        emoji = get_emoji(int(finds[i]))
+        if emoji is not None:
+            return emoji
+        i += 1
+
     return None
