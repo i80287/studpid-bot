fd80523edc26710953eeb9e9b91e7acb76cdb8e9
i80287 <addmlbx@gmail.com>
2024-09-09 00:23:37 +0300

    replace deprecated datetime.utcnow() with datetime.now(datetime.UTC)

--- storebot/Tools/logger.py
+++ storebot/Tools/logger.py
@@ -1,5 +1,5 @@
 from aiofiles import open
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 
 from ..constants import CWD_PATH
 DIR_PATH = CWD_PATH + '/'
@@ -10,14 +10,14 @@ _TZ_OFFSET = timedelta(hours=3.0)
 async def write_log_async(filename: str, *report: str) -> None:
     assert isinstance(filename, str) and filename.endswith(".log")
     async with open(DIR_PATH + filename, mode="a+", encoding="utf-8") as f:
-        await f.write(f"[{datetime.utcnow() + _TZ_OFFSET}] [{'] ['.join(map(str, report))}]\n")
+        await f.write(f"[{datetime.now(UTC) + _TZ_OFFSET}] [{'] ['.join(map(str, report))}]\n")
 
 async def write_one_log_async(filename: str, report: str) -> None:
     assert isinstance(filename, str) and filename.endswith(".log")
     async with open(DIR_PATH + filename, mode="a+", encoding="utf-8") as f:
-        await f.write(f"[{datetime.utcnow() + _TZ_OFFSET}] {report}\n")
+        await f.write(f"[{datetime.now(UTC) + _TZ_OFFSET}] {report}\n")
 
 async def write_guild_log_async(filename: str, guild_id: int, report: str) -> None:
     assert isinstance(filename, str) and filename.endswith(".log")
     async with open(GUILD_LOGS_PATH.format(guild_id) + filename, mode="a+", encoding="utf-8") as f:
-        await f.write(f"[{datetime.utcnow() + _TZ_OFFSET}] {report}\n")
+        await f.write(f"[{datetime.now(UTC) + _TZ_OFFSET}] {report}\n")
